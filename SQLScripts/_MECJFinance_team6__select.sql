583984dccca5e8d30e433d2504fc2bfb1c38e097
eproduct16 <100736664+eproduct16@users.noreply.github.com>
2022-03-01 21:18:37 -0800

    Update _MECJFinance_team6__select.sql

--- SQLScripts/_MECJFinance_team6__select.sql
+++ SQLScripts/_MECJFinance_team6__select.sql
@@ -1,11 +1,15 @@
-SELECT Billid
-FROM BILL
-WHERE Duedate = '2022-01-01 12:00:00';
+SELECT Title
+FROM BILL b, EXPENSE e
+WHERE Duedate = '2022-01-01 12:00:00'AND b.Userid=e.Userid;
 
-SELECT Userid
-FROM USER
-WHERE Name = 'Alice Allison';
+SELECT Totalamount, Currentamount, Description
+FROM GOAL g, EXPENSE e
+WHERE Category = 1 AND g.UserId = e.UserId;
 
 SELECT Description
+FROM EXPENSE 
+WHERE Title = 'Shell' OR Title = 'Grocery Store';
+
+SELECT Description 
 FROM EXPENSE
-WHERE Title = 'Shell';
+WHERE Amount BETWEEN 100 AND 200;
